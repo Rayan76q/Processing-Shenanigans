13642582f746f2df9125c74a50df88178b3bd22e
Rayan LALAOUI <rayan.lalaoui.768@gmail.com>
2024-03-27 15:04:25 +0100

    Ajout d'un shader basique , vallée colorié en vert et lignes de niveaux

--- myFragmentShader.glsl
+++ myFragmentShader.glsl
@@ -3,21 +3,24 @@ precision mediump float;
 precision mediump int;
 #endif
 
+uniform sampler2D texture;
 
 varying vec4 vertColor;
-varying vec3 vertNormal;
-varying vec3 vertLightDir;
-varying vec4 vertexCoord;
-
+varying vec4 vertTexCoord;
+varying float z;
 
 void main() {
-  vec4 color;
-  color = vec4(1.0);
-  float z = vertexCoord.z;
-  if( floatBitsToInt(z)%50<=10){
-    color = vec4(vec3(1.0), 1.0);
-  }
-  
+  float zmin = -202.09592, zmax = -179.59933;
 
-  gl_FragColor = color * vertColor;
-} 
+  float vallee = (zmax-zmin)/5 + zmin ;
+  vec4 color = vec4(1.0);
+  if((-int(z*2))%5 == 0){
+    color = vec4(vec3(0.4) , 1.0);
+  }
+  if(z<=vallee ){
+    color = vec4(0.25 , 2.0,0.25 , 1.0);
+  }
+
+
+  gl_FragColor = texture2D(texture, vertTexCoord.st) * vertColor * color;
+}
